d1412bcd16400e0f4b2bcd788053e02f81d475e5
Angel Ferraez <angel.ferre.5000a@hotmail.com>
2025-10-25 14:52:46 -0600

    feat(lib): creación de utils para controlar la lógica

---
+++ src/lib/utils.ts
@@ -0,0 +1,34 @@
+export const getHref = (nombreSala: string) => {
+  switch (nombreSala) {
+    case "Aves tropicales":
+      return "/galeria-de-arte/aves-tropicales";
+    case "Corredor de las máscaras":
+      return "/galeria-de-arte/corredor-de-las-mascaras";
+    case "Figura humana":
+      return "/galeria-de-arte/figura-humana";
+    case "Lool Saasil":
+      return "/galeria-de-arte/lool-saasil";
+    case "Mitos y leyendas del Mayab":
+      return "/galeria-de-arte/mitos-y-leyendas-del-mayab";
+    case "Mujer maya":
+      return "/galeria-de-arte/mujer-maya";
+    case "Palapa de las mariposas":
+      return "/galeria-de-arte/palapa-de-las-mariposas";
+    case "Pitahayas de Adolfo":
+      return "/galeria-de-arte/pitahayas-de-adolfo";
+    case "Pitahayas-Dragon fruit":
+      return "/galeria-de-arte/pitahayas-dragon-fruit";
+    case "Plantas, flores y frutos":
+      return "/galeria-de-arte/plantas-flores-y-frutos";
+    case "Retratos":
+      return "/galeria-de-arte/retratos";
+    case "Corredor":
+      return "/galeria-de-arte/corredor";
+    case "Comedor":
+      return "/galeria-de-arte/comedor";
+    case "Cocina":
+      return "/galeria-de-arte/cocina";
+    case "Pasillo de la planta baja":
+      return "/galeria-de-arte/pasillo-de-la-planta-baja";
+  }
+};
