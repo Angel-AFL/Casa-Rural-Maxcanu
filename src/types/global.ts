3c01d24cc119d0ac5250d4f2b002c9d81997dde0
elTuercas0309 <gerardojaribgtz@gmail.com>
2025-11-14 07:23:41 -0600

    feat(archivos):modificación de archivos

--- src/types/global.ts
+++ src/types/global.ts
@@ -10,8 +10,10 @@ export interface ImgCabanaHabitacion {
   imagen: string;
 }
 
-
-// Catálogo de cabañas/habitaciones
+//nombres de cabañas/habitaciones
+export interface CatCabanaHabitacion {
+  nombre: string;
+}
 
 
 // Disponibilidad de hospedaje
@@ -19,6 +21,8 @@ export interface DisponibilidadHospedaje {
   fecha_entrada: string;
   fecha_salida: string;
 }
+
+//catalogo galeria de arte
 export interface CatGaleriaArte {
   id: number;
   sala: string;
@@ -27,4 +31,47 @@ export interface CatGaleriaArte {
   tipo: string;
   imagen: string;
   created_at: string;
+}
+
+
+//registro hospedaje
+export interface HospedajePayload {
+  nombre: string;
+  apellido: string;
+  correo: string;
+  telefono: string;
+  habitacion_cabana: string;
+  fecha_entrada: string; 
+  fecha_salida: string;
+}
+
+export interface HospedajeResponse {
+  folio: string;
+  nombre: string;
+  apellido: string;
+  fecha_entrada: string; 
+  fecha_salida: string;
+}
+
+//bicicleta renta
+export interface RentaBiciPayload {
+  nombre: string;
+  apellido: string;
+  telefono: string;
+}
+
+export interface RentaBiciResponse {
+  folio_generado: string;
+}
+
+// devolucion de bicicletas
+export interface DevolucionBiciPayload {
+  folio: string;
+}
+
+export interface DevolucionBiciResponse {
+  folio_actualizado: string;
+  nombre_cliente: string;
+  estatus_nuevo: string;
+  fecha_devolucion_actualizada: string; 
 }
