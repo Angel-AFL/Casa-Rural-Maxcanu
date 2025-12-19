14267be5dc6013c86ace0544377f9a19389b5033
elTuercas0309 <gerardojaribgtz@gmail.com>
2025-12-18 20:24:13 -0600

    feat(modifiet): sintaxis ordenada

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,20 +5,16 @@ import tailwindcss from "@tailwindcss/vite";
 
 import react from "@astrojs/react";
 
-// ⚠️ Importa el adaptador de Node.js, ya que lo necesitarás para output: 'server'
 import node from "@astrojs/node"; 
 
-// https://astro.build/config
 export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
   integrations: [react()],
   
-  // 🚨 LÍNEA CRUCIAL: Cambia el modo de salida a server
   output: 'server',
   
-  // 🚨 ADAPTADOR: Debes especificar qué adaptador de Node usarás para el servidor
   adapter: node({
       mode: 'standalone',
   }),
