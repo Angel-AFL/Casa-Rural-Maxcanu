f051ab13b68913d60467ace58240ad01105c4b3d
Angel Ferraez <angel.ferre.5000a@hotmail.com>
2025-12-28 13:36:32 -0600

    refactor(cat_galeria_arte): Se mejoró la función del llamado a los endpoints

--- src/pages/api/cat_galeria_arte.ts
+++ src/pages/api/cat_galeria_arte.ts
@@ -3,192 +3,121 @@ import type { CatGaleriaArte } from "../../types/global";
 const apikey = import.meta.env.SUPABASE_KEY;
 const authorization = `Bearer ${import.meta.env.SUPABASE_KEY}`;
 
-export async function getCatGaleriaArte(): Promise<
-  CatGaleriaArte[] | undefined
-> {
+const getArtsData = async (
+  url: string
+): Promise<CatGaleriaArte[] | undefined> => {
   try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
+    const response = await fetch(url, {
+      headers: {
+        apikey,
+        authorization: authorization,
+      },
+    });
 
     if (!response.ok) {
-      throw new Error(
-        `Error de Supabase: ${response.status} ${response.statusText}`
-      );
+      throw new Error(`Error en la petición: ${response.statusText}`);
     }
 
     const data = (await response.json()) as CatGaleriaArte[];
-    console.log("Datos de galerias de arte obtenidos:", data);
     return data;
   } catch (error) {
-    console.error("Error fetching galerias de arte:", error);
+    console.error(error);
+    return undefined;
   }
+};
+
+export function getAvesTropicales() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Aves%20tropicales"
+  );
 }
 
-export async function getAvesTropicales(): Promise<
-  CatGaleriaArte[] | undefined
-> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Aves%20tropicales",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Aves Tropicales:", error);
-  }
+export function getFiguraHumana() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Figura%20humana"
+  );
 }
 
-export async function getFiguraHumana(): Promise<CatGaleriaArte[] | undefined> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Figura%20humana",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Figura Humana:", error);
-  }
+export function getCorredorDeLasMascaras() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Corredor%20de%20las%20máscaras"
+  );
 }
 
-export async function getCorredorDeLasMascaras(): Promise<
-  CatGaleriaArte[] | undefined
-> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Corredor%20de%20las%20máscaras",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Corredor de las máscaras", error);
-  }
+export function getLolSaasil() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Lol%20Saasil"
+  );
 }
 
-export async function getLolSaasil(): Promise<CatGaleriaArte[] | undefined> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Lol%20Saasil",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Lol Saasil", error);
-  }
+export function getMitosYLeyendasDelMayab() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Mitos%20y%20leyendas%20del%20Mayab"
+  );
 }
 
-export async function getMitosYLeyendasDelMayab(): Promise<
-  CatGaleriaArte[] | undefined
-> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Mitos%20y%20leyendas%20del%20Mayab",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Mitos y leyendas del Mayab", error);
-  }
+export function getCocina() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Cocina"
+  );
 }
 
-export async function getMujerMaya(): Promise<CatGaleriaArte[] | undefined> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Mujer%20maya",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Mujer Maya", error);
-  }
+export function getComedor() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Comedor"
+  );
 }
 
-export async function getPalaDeLasMariposas(): Promise<
-  CatGaleriaArte[] | undefined
-> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Palapa%20de%20las%20mariposas",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
-
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Palapa de las Mariposas", error);
-  }
+export function getCorredorDelPozo() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Corredor%20del%20pozo"
+  );
 }
 
-export async function getPitahayasDeAdolfo(): Promise<
-  CatGaleriaArte[] | undefined
-> {
-  try {
-    const response = await fetch(
-      "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Pitahayas%20de%20Adolfo",
-      {
-        headers: {
-          apikey: apikey,
-          authorization: authorization,
-        },
-      }
-    );
+export function getMujerMaya() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Mujer%20maya"
+  );
+}
 
-    const data = (await response.json()) as CatGaleriaArte[];
-    return data;
-  } catch (error) {
-    console.error("Error fetching Pitahayas De Adolfo", error);
-  }
+export function getPalapaDeLasMariposas() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Palapa%20de%20las%20mariposas"
+  );
+}
+
+export function getPasilloDeLaPLantaBaja() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Pasillo%20de%20planta%20baja"
+  );
+}
+
+export function getPitahayasDeAdolfo() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Pitahayas%20de%20Adolfo"
+  );
+}
+
+export function getPitahaysDragonFruit() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Pitahayas-Dragon%20fruit"
+  );
+}
+
+export function getPlantasFloresYFrutos() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Plantas,%20flores%20y%20frutos"
+  );
+}
+
+export function getRetratos() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Retratos"
+  );
+}
+
+export function getVarios() {
+  return getArtsData(
+    "https://jrrwlqhzxlfcsbaanzzq.supabase.co/rest/v1/cat_galeria_arte?select=*&sala=eq.Varias"
+  );
 }
